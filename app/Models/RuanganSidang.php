936054376934c0adbcc5fde8fa70439173012656
MariaIneFebrianti <magangmaria29@gmail.com>
2025-02-13 15:28:01 +0700

    feat: model ruangan sidang 13 02 2025

---
+++ app/Models/RuanganSidang.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class RuanganSidang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ruangan_sidang';
+    protected $fillable = [
+        'prodi_id',
+        'nama_ruangan',
+    ];
+
+    public function programStudi()
+    {
+        return $this->belongsTo(ProgramStudi::class, 'prodi_id');
+    }
+}
