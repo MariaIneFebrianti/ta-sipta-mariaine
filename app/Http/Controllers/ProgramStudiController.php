5809ffd3ffb0e8b29ca519f3a976ca4eaecff159
MariaIneFebrianti <132965857+MariaIneFebrianti@users.noreply.github.com>
2025-04-14 16:52:43 +0700

    update: controller Prorgam Studi 14 04 2025

--- app/Http/Controllers/ProgramStudiController.php
+++ app/Http/Controllers/ProgramStudiController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\ProgramStudi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class ProgramStudiController extends Controller
 {
     public function index()
     {
-        $programStudi = ProgramStudi::all();
-        return view('program_studi.index', compact('programStudi'));
+        $userRole = Auth::user()->role;
+        $programStudi = ProgramStudi::paginate(5);
+
+        return view('program_studi.index', compact('programStudi', 'userRole'));
     }
 
     public function store(Request $request)
@@ -20,12 +24,8 @@ class ProgramStudiController extends Controller
             'nama_prodi' => 'required|string|max:255'
         ]);
         ProgramStudi::create($request->all());
-        return redirect()->route('program_studi.index')->with('success', 'Program Studi berhasil ditambahkan');
-    }
 
-    public function show(string $id)
-    {
-        //
+        return redirect()->route('program_studi.index')->with('success', 'Program Studi berhasil ditambahkan');
     }
 
     public function update(Request $request, string $id)
@@ -34,10 +34,26 @@ class ProgramStudiController extends Controller
             'kode_prodi' => 'required|string|max:255',
             'nama_prodi' => 'required|string|max:255'
         ]);
-
         $programStudi = ProgramStudi::findOrFail($id);
         $programStudi->update($request->all());
+
         return redirect()->route('program_studi.index')->with('success', 'Program Studi berhasil diperbarui');
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+
+        // Mengambil data pengguna berdasarkan pencarian kode prodi atau nama prodi
+        $programStudi = ProgramStudi::when($search, function ($query) use ($search) {
+            return $query->where(function ($query) use ($search) {
+                $query->where('kode_prodi', 'like', "%$search%")
+                    ->orWhere('nama_prodi', 'like', "%$search%");
+            });
+        })
+            ->paginate(5);
+
+        return view('program_studi.index', compact('programStudi'));
     }
 
     public function destroy(string $id)
