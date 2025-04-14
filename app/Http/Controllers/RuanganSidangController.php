f0741459237357535c164d390d9a6b39accda100
MariaIneFebrianti <132965857+MariaIneFebrianti@users.noreply.github.com>
2025-04-14 16:52:57 +0700

    update: controller Ruangan Sidang 14 04 2025

--- app/Http/Controllers/RuanganSidangController.php
+++ app/Http/Controllers/RuanganSidangController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\ProgramStudi;
 use App\Models\RuanganSidang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class RuanganSidangController extends Controller
 {
     public function index()
     {
-        $ruanganSidang = RuanganSidang::with('programStudi')->get();
+        $userRole = Auth::user()->role;
+        $ruanganSidang = RuanganSidang::with('programStudi')->paginate(5);
         $programStudi = ProgramStudi::all();
-        return view('ruangan_sidang.index', compact('ruanganSidang', 'programStudi'));
+
+        return view('ruangan_sidang.index', compact('ruanganSidang', 'programStudi', 'userRole'));
     }
 
     public function store(Request $request)
@@ -21,14 +25,9 @@ class RuanganSidangController extends Controller
             'nama_ruangan' => 'required|string|max:255',
             'prodi_id' => 'required|exists:program_studi,id',
         ]);
-
         RuanganSidang::create($request->all());
-        return redirect()->route('ruangan_sidang.index')->with('success', 'Program Studi berhasil ditambahkan');
-    }
 
-    public function show(string $id)
-    {
-        //
+        return redirect()->route('ruangan_sidang.index')->with('success', 'Program Studi berhasil ditambahkan');
     }
 
     public function update(Request $request, string $id)
@@ -37,16 +36,35 @@ class RuanganSidangController extends Controller
             'nama_ruangan' => 'required|string|max:255',
             'prodi_id' => 'required|exists:program_studi,id',
         ]);
-
         $ruanganSidang = RuanganSidang::findOrFail($id);
         $ruanganSidang->update($request->all());
+
         return redirect()->route('ruangan_sidang.index')->with('success', 'Program Studi berhasil diperbarui');
+    }
+
+    public function search(Request $request)
+    {
+        $programStudi = ProgramStudi::all();
+        $search = $request->input('search');
+
+        // Mengambil data pengguna berdasarkan pencarian nama ruangan atau program studi
+        $ruanganSidang = RuanganSidang::when($search, function ($query) use ($search) {
+            return $query->where(function ($query) use ($search) {
+                $query->where('nama_ruangan', 'like', "%$search%")
+                    ->orWhereHas('programStudi', function ($query) use ($search) {
+                        $query->where('nama_prodi', 'like', "%$search%");
+                    });
+            });
+        })->paginate(5);
+
+        return view('ruangan_sidang.index', compact('ruanganSidang', 'programStudi'));
     }
 
     public function destroy(string $id)
     {
         $ruanganSidang = RuanganSidang::findOrFail($id);
         $ruanganSidang->delete();
+
         return redirect()->route('ruangan_sidang.index')->with('success', 'Program Studi berhasil dihapus');
     }
 }
