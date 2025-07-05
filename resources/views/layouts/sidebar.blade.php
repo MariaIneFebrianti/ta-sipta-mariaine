a7836050591cf0cc8af08014a809f7941ee967b9
MariaIneFebrianti <132965857+MariaIneFebrianti@users.noreply.github.com>
2025-07-05 19:43:47 +0700

    update: view

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -231,7 +231,7 @@
                     @elseif (auth()->user()->role === 'Dosen' && auth()->user()->dosen && auth()->user()->dosen->jabatan === 'Koordinator Program Studi' || auth()->user()->role === 'Dosen' && auth()->user()->dosen && auth()->user()->dosen->jabatan === 'Super Admin')
                         <a href="{{ route('pendaftaran_sidang.index_kaprodi') }}" class="menu-button flex items-center p-2 rounded-lg {{ request()->routeIs('pendaftaran_sidang.index_kaprodi') ? 'bg-blue-600 text-white' : 'bg-white text-black'  }}">
                     @endif
-                        <svg class="w-7 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 {{ request()->routeIs('pendaftaran_sidang.*') ? 'text-white' : 'text-gray-500' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <svg class="w-7 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 {{ request()->routeIs('pendaftaran_sidang.index', 'pendaftaran_sidang.index_kaprodi') ? 'text-white' : 'text-gray-500' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M9 2.221V7H4.221a2 2 0 0 1 .365-.5L8.5 2.586A2 2 0 0 1 9 2.22ZM11 2v5a2 2 0 0 1-2 2H4v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-7Z" clip-rule="evenodd"/>
                         </svg>
                         <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Pendaftaran Sidang</span>
