35f64862788a96a8b4897e239e931818c685cd52
MariaIneFebrianti <132965857+MariaIneFebrianti@users.noreply.github.com>
2025-07-07 20:56:21 +0700

    update component

--- resources/views/components/alert-global.blade.php
+++ resources/views/components/alert-global.blade.php
@@ -2,6 +2,10 @@
     <x-alert type="success" :message="session('success')" />
 @endif
 
+@if (session('error'))
+    <x-alert type="error" :message="session('error')" />
+@endif
+
 @if ($errors->any())
     @foreach ($errors->all() as $error)
         <x-alert type="error" :message="$error" />
