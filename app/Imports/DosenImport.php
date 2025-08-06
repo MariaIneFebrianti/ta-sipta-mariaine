88b31ef498bc0bb6c68d28d4e9a57a29a05f72b4
MariaIneFebrianti <132965857+MariaIneFebrianti@users.noreply.github.com>
2025-08-06 11:10:06 +0700

    update: import

--- app/Imports/DosenImport.php
+++ app/Imports/DosenImport.php
@@ -75,41 +75,4 @@ class DosenImport implements ToCollection
             session()->flash('error', 'Gagal mengimpor data dosen: ' . $e->getMessage());
         }
     }
-
-    // public function collection(Collection $collection)
-    // {
-    //     try {
-    //         DB::transaction(function () use ($collection) {
-    //             foreach ($collection as $key => $row) {
-    //                 // Skip header row
-    //                 if ($key === 0) continue;
-
-    //                 // Buat user baru
-    //                 $user = User::create([
-    //                     'name' => $row[0],
-    //                     'email' => $row[1],
-    //                     'email_verified_at' => now(),
-    //                     'password' => Hash::make('11111111'),
-    //                     'role' => 'Dosen',
-    //                     'created_at' => now(),
-    //                     'updated_at' => now(),
-    //                 ]);
-
-    //                 // Simpan data dosen dengan user_id dari user yang baru dibuat
-    //                 Dosen::create([
-    //                     'user_id' => $user->id,
-    //                     'nama_dosen' => $row[0],
-    //                     'nip' => $row[2],
-    //                     'tempat_lahir' => $row[3],
-    //                     'tanggal_lahir' => $row[4],
-    //                     'jenis_kelamin' => $row[5],
-    //                 ]);
-    //             }
-    //         });
-
-    //         session()->flash('success', 'Data dosen berhasil diimpor.');
-    //     } catch (\Exception $e) {
-    //         session()->flash('error', 'Gagal mengimpor data dosen: ' . $e->getMessage());
-    //     }
-    // }
 }
